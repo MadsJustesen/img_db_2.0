b1359f848cfe136be5df197bd32454916fbe381c
MadsJustesen <majus12@student.sdu.dk>
2016-03-16 23:03:15 +0100

    Header layout

---
+++ views/header.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+	<ul>
+		<li class="active"><a href="index.php">Home</a></li>
+		<li><a href="users.php">Brugere</a></li>
+		<li><a href="add_user.php">Tilføj bruger</a></li>
+		<li><a href="my_images.php">Mine Billeder</a></li>
+		<li><a href="upload_image.php">Upload Billede</a></li>
+		<li><a href="log_out.php">Log Ud</a></li>
+	</ul>
+</div>
