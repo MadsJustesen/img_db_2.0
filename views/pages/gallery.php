21b28e71c8c917dbfdee4321157e564953fc48fd
MadsJustesen <majus12@student.sdu.dk>
2016-03-21 18:10:35 +0100

    titles on images

--- views/pages/gallery.php
+++ views/pages/gallery.php
@@ -6,12 +6,19 @@ require '../views/header.php';
 	<div class="container-header">
 		<h2>My images</h2>
 	</div>
-	<table>
+	<table  class="image-table">
 		<?php 
 		$i = 1;
 		echo '<tr>';
 		foreach ($images as $image) {
-			echo '<td class="image-table"><img src="data:image/jpeg;base64,' . base64_encode( $image["image"] ) . '"/></td>';
+			echo '<td>
+			<div class="image-container" style="overflow:hidden;">
+				<div class="image-header">
+					<h3>' . $image["title"] . '</h3>
+				</div>
+				<img src="data:image/jpeg;base64,' . base64_encode( $image["image"] ) . '"/>
+			</div>
+			</td>';
 
 			if($i % 4 === 0){
 				echo "</tr><tr>";
