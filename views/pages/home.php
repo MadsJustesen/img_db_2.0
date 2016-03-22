064a4abd40bfa1b04458bafe768e18c5cc6ce228
MadsJustesen <majus12@student.sdu.dk>
2016-03-22 16:03:55 +0100

    shadows, color changes

--- views/pages/home.php
+++ views/pages/home.php
@@ -6,8 +6,6 @@ require '../views/header.php';
 	<div class="container-header">
 		<h2>Latest images</h2>
 	</div>
-	<p class= "image-title">En super heeelt</p>
-	<img src="assets/img/superhero_gif.gif" class="latest-images">
 </div>
 
 <?php 
